f4e40c9f1fa7ea5856764effd10820f4eddfccbc
KIM HYUN SOO <rlagustn9149@naver.com>
2021-07-19 17:35:10 +0900

    2021/07/19

---
+++ 그리디/_1541.java
@@ -0,0 +1,30 @@
+package 그리디;
+
+import java.io.*;
+import java.util.*;
+
+public class _1541 {
+    
+    public static int max=100000000;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer sub = new StringTokenizer(br.readLine(),"-");
+        int sum=max;
+        while(sub.hasMoreTokens()){
+            int tmp =0;
+            StringTokenizer add=new StringTokenizer(sub.nextToken(),"+");
+            
+            while(add.hasMoreTokens()){
+                tmp+=Integer.parseInt(add.nextToken());
+            }
+
+            if(sum==max){
+                sum=tmp;
+            }else{
+                sum-=tmp;
+            }
+        }
+        System.out.println(sum);
+    }
+}
