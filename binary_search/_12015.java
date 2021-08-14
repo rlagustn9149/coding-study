b03cd059dc0ca6a0d50221cedbc195c5ba98a040
rlagustn9149 <rlagustn9149@naver.com>
2021-08-15 01:39:45 +0900

    2021/08/14

--- binary_search/_12015.java
+++ binary_search/_12015.java
@@ -1,16 +1,42 @@
 package binary_search;
+
 import java.io.*;
 import java.util.*;
+
 public class _12015 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++) arr[i]=Integer.parseInt(st.nextToken());
-        
-        for(int i=0; i<n; i++){
-            
-        }
+  
+  static ArrayList<Integer> arr = new ArrayList<>();
+  
+  public static void binarySearch(int k, int start, int end){
+    
+    while(start<end){
+      
+      int mid=(start+end)/2;
+      
+      if(arr.get(mid)<k) start=mid+1;
+      else end = mid;  
     }
+    arr.set(end,k);
+
+  }
+  
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n= Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    arr.add(Integer.parseInt(st.nextToken()));
+    
+    for(int i=0; i<n-1; i++){
+
+      int k = Integer.parseInt(st.nextToken());
+      if(k>arr.get(arr.size()-1)){
+        arr.add(k);
+      }else{
+        binarySearch(k,0,arr.size()-1);
+      }
+    }
+
+    System.out.print(arr.size());
+  }
 }
