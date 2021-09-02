c0ea6b3024020c7134be47e1d28838c28036b1fc
KIM HYUN SOO <rlagustn9149@naver.com>
2021-09-02 11:38:09 +0900

    2021/09/02

--- BOJ/binary_search/_1300.java
+++ BOJ/binary_search/_1300.java
@@ -1,18 +1,31 @@
-package binary_search;
+package BOJ.binary_search;
 import java.io.*;
+import java.util.*;
 public class _1300 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n= Integer.parseInt(br.readLine());
         int k= Integer.parseInt(br.readLine());
-        
-        long start =1;
-        long end =k;
-        
-        while(start<=end){
-            long mid=(start+end)/2;
+      
+        long left=1;
+        long right=k;
+        long answer=0;
+        while(left<=right){
+            long mid = (left+right)/2;
             long cnt=0;
-            
+            for(int i=1; i<=n; i++){
+                    cnt+=Math.min(mid/i,n);
+            }
+            if(cnt<k){
+                left=mid+1;
+            }else{
+                answer=mid;
+                right=mid-1;
+            }
+
         }
+        System.out.print(answer);
+     
+        
     }
 }
